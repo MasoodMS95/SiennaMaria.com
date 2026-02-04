0bb07a7b7e0fc3bd1f3e8889c03f5156c2f1a086
Masood <masoodsaaed@gmail.com>
2026-02-04 11:17:53 -0800

    vite react setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Routes>
+      <Route/>
+    </Routes>
+  )
+}
+
+export default App

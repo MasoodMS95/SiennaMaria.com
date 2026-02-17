72b07e17a85943a22ab9f4eebbe07fa3e49deefe
Masood <masoodsaaed@gmail.com>
2026-02-16 23:50:55 -0800

    Created and designed Navbar

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -1,27 +1,30 @@
+import React from 'react';
 import './NavBar.css'
 import { useNavigate  } from 'react-router-dom';
+import image from '../../assets/sienna_banner.png'
 export default function NavBar(){
-    let history = useNavigate();
+
+    let navigate = useNavigate();
+
     return (
-        <div className='nav-bar'>
-            <ul>
-                <li onClick={()=>{
-                    console.log('clicked')
-                    history.push('/')}
-                    } 
+        <React.Fragment>
+            <img className='hero-banner' src={image} />
+            <ul className='nav-bar'>
+                <li onClick={()=>navigate("/")}
                     className="home-nav-button nav-button">
                     <i id="home-image" className="fa-solid fa-house"></i>
                     <span>Home</span>
                 </li>
-                <li className="link-tree-nav-button nav-button">
+                <li onClick={()=>navigate("/socials")}
+                    className="link-tree-nav-button nav-button">
                     <i className="fa-solid fa-house"></i>
                     <span>Links</span>
                 </li>
-                <li className="home-nav-button nav-button">
+                <li className="home-nav-button nav-button last-item">
                     <i className="fa-solid fa-house"></i>
                     <span>Home</span>
                 </li>
             </ul>
-        </div>
+        </React.Fragment>
     )
 }

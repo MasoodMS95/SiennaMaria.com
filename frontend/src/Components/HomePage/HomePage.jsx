153789d8cb3adfe88aa88b3fa68583e17a64d83d
Masood <masoodsaaed@gmail.com>
2026-02-16 23:40:38 -0800

    Removed Navbar from homepage and moved to app.jsx

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './HomePage.css'
-import NavBar from '../NavBar/NavBar'
 import LatestVideoPlayer from './LatestVideoOnYoutube/LatestVideoPlayer'
 
 export default function HomePage(){
+    
     return (
         <React.Fragment>
-            <NavBar/>
             <LatestVideoPlayer/>
         </React.Fragment>
     )
